02ad722e07c25bdc9d06f608b13210a0e1ea8720
edris gemtessa <edogemtessa@gmail.com>
2021-01-22 18:13:02 -0600

    Define the function and have it accept the Budget_data

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,1 +1,13 @@
 #Main Program for PyBank
+import os
+import csv
+budget_data_csv =os.path.join("..", "Resources", "budget_data.csv")
+
+def BudgetAnalysis(budgetdata):
+    Total_Month = int(budgetdata[0])
+    Total_Loss_Profit = float(budgetdata[1])
+    Average_Loss_Profit = float(budgetdata[2])
+    Max_Loss_profit = float(budgetdata[3])
+    Min_Loss_Profit = float(budgetdata[4])
+with open(budget_data_csv) as csv_file:
+    
