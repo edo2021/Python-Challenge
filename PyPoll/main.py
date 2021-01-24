04bbf2b15b4249ddd53605b74cc24bd622093493
edris gemtessa <edogemtessa@gmail.com>
2021-01-24 15:01:49 -0600

    create list and variable

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -7,3 +7,14 @@ import csv
  # Set the path for the CSV file 
 
 PyPollcsv = os.path.join("Resources","election_data.csv")
+
+#Create the lists to store data. 
+
+
+candidatelist = []
+unique_candidate = []
+vote_count = []
+vote_percent = []
+
+#create variable and initalize
+count = 0
