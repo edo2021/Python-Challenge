39ba8b60198fff6290c722791eca1e202e1041c9
edris gemtessa <edogemtessa@gmail.com>
2021-01-24 12:54:19 -0600

    Printing results

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,7 +5,9 @@ import csv
 
 #Gettting the file Path
 
-Budget_datacsv = os.path.join("Resource","budget_data.csv")
+#budget_datacsv = os.path.join("Resource","budget_data.csv")
+
+Datacsv = os.path.join("Resources","budget_data.csv")
 
 #List to store Data
 profit = []
@@ -21,8 +23,9 @@ initial_profit = 0
 
 # Opening the csv file from the path for reading
 
-with open (Budget_datacsv) as csvfile:
-    csvreader = csv.reader(csvfile, delimite = ",")
+#with open (budget_datacsv) as csvfile:
+with open(Datacsv, newline="") as csvfile:
+    csvreader = csv.reader(csvfile, delimiter = ",")
 
     # Reading the heeading
     cvs_header = next(csvreader)
@@ -43,8 +46,31 @@ with open (Budget_datacsv) as csvfile:
 
         average_change_profits = (total_change_profits/count)
 
+        #Finding Greatest increase and Decrease
+        greatest_increase_profits = max(monthly_changes)
+        greatest_decrease_profits = min(monthly_changes)
+        increase_date = date[monthly_changes.index(greatest_increase_profits)]
+        decrease_date = date[monthly_changes.index(greatest_decrease_profits)]
+
+    
+    
+    print("Total Months: " + str(count))
+    print("Total Profits: " + "$" + str(total_profit))
+
+    print("Average Change: " + "$" + str(int(average_change_profits)))
+    
+    print("Greatest Increase in Profits: " + str(increase_date) + " ($" + str(greatest_increase_profits) + ")")
+    print("Greatest Decrease in Profits: " + str(decrease_date) + " ($" + str(greatest_decrease_profits)+ ")")
+    
+       
+      
 
 
 
 
 
+
+
+
+
+        
