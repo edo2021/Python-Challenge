8119283ac9deab7b54c8f69fe0a24b5531abdb3e
edris gemtessa <edogemtessa@gmail.com>
2021-01-24 11:31:47 -0600

    creating the variables and opening the file for reading

--- PyBank/main.py
+++ PyBank/main.py
@@ -12,6 +12,20 @@ profit = []
 monthly_changes = []
 date = []
 
+ #creating and Initialize the variables 
+ 
+count = 0
+total_profit = 0
+total_change_profits = 0
+initial_profit = 0
+
+# Opening the csv file from the path for reading
+
+with open (Budget_datacsv) as csvfile:
+    csvreader = csv.reader(csvfile, delimite = ",")
+
+    # Reading the heeading
+    cvs.csv_header = next(csvreader)
 
 
 
